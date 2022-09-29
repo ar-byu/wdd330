372e5c55d29d9090c07df9745dce1d0eff3cd7ff
ar-byu <103947632+ar-byu@users.noreply.github.com>
2022-09-28 22:10:35 -0400

    Added Week 3 notes and updated links object

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ const links = [
     {
         label: "Week 2 Team Activity",
         url: "week2/index.html"
+    },
+    {
+        label: "Week 3 Notes",
+        url: "week3/index.html"
     }
 ]
 
